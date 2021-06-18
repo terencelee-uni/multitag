8c0be9b92347086f4be6d63e7166c4d811f2fffe
Terence Lee <terencelee.uni@gmail.com>
2021-06-17 22:03:15 -0700

    Removed normalization tranform as it messed up classification

--- functions.py
+++ functions.py
@@ -12,10 +12,7 @@ from flask import Flask, jsonify, request
 def transform_image(image_bytes):
     my_transforms = transforms.Compose([transforms.Resize(255),
                                         transforms.CenterCrop(224),
-                                        transforms.ToTensor(),
-                                        transforms.Normalize(
-                                            [0.485, 0.456, 0.406],
-                                            [0.229, 0.224, 0.225])])
+                                        transforms.ToTensor()])
     image = Image.open(io.BytesIO(image_bytes))
     return my_transforms(image).unsqueeze(0)
 
